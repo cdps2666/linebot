ec53a5a303cf18e2b4f3be56f2ad0113106e3240
cdps2666@gmail.com <cdps2666@gmail.com>
2023-12-16 13:08:52 +0800

    add chatbot.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
+import chatbot
 #line token
 channel_access_token = 'G3l1mLCs/oQ0b3K0YywjYO/1NJN3MCpkItVwSVmxBLzjav9Er6XZ38YswKFVybrURmD0VX5uZIJrakrbZRRJtZdNOAwKpvPMJuIeCM7WZ3NF3PmnYcMKpqBDO9elbFW1UEctFNb5LZNLk3Za46NjvAdB04t89/1O/w1cDnyilFU='
 channel_secret = 'ba5cbc8df30a57ea62f2e6acc7938671'
@@ -31,7 +31,7 @@ def callback():
 def handle_message(event):
     #echo
     # msg= event.message.text
-    msg='https://www.google.com/maps/dir/24.8244389,121.7022625/24.6748671,121.675608/24.6118089,121.6365653/24.7603435,121.7534132/24.8434334,121.7820505/24.8164107,121.8217512/24.8686561,121.8327408/24.6427251,121.7330825/24.7139177,121.6885279/24.7536907,121.750255/@24.80845,121.6350792,11.26z?entry=ttu'
+    msg = chatbot.get_response(event.message.text)
     message = TextSendMessage(text=msg)
     line_bot_api.reply_message(event.reply_token,message)
 
---
+++ chatbot.py
@@ -0,0 +1,57 @@
+import re
+import json
+
+
+def extract_urls(text):
+    # 定義簡單的正規表達式模式，以匹配簡單的URL格式
+    url_pattern = re.compile(r'https?://\S+')
+
+    # 使用 findall 方法找到所有匹配的網址
+    urls = re.findall(url_pattern, text)
+
+    return urls
+
+def get_map_url(video_url):
+
+    obj = {
+        "url": "https://tinyurl.com/yssopd74",
+        "location_list": [ 
+            {'order':1,'location':'地點名稱','url':'https://tinyurl.com/yl64yqu9'},
+            {'order':2,'location':'地點名稱','url':'https://tinyurl.com/yl64yqu9'}  
+        ],
+        "status":True
+    }
+
+    return obj
+
+def get_response(input_message):
+    msgs =[]
+
+    result_urls = extract_urls(input_message)
+    if len(result_urls) >0 :
+        result = get_map_url(result_urls[0])
+        if ( result ) :
+            if( result['status'] == True) :
+                url = result['url']
+                msg = f"💡 根據您提供的影片，為您分析整理的旅遊行程網址:\n{url}"
+                msgs.append(msg)
+
+                msg = "\n⭐ 並列出以下各景點的位置\n"
+                for item in result['location_list']:
+                    order = item['order']
+                    item_location = item['location']
+                    item_url= item['url']
+                    msg += f"{order}. {item_location} {item_url}\n"
+                msgs.append(msg)
+            else :
+                msg ="🙇 很抱歉，出了點問題，無法為您分析旅遊行程，可以試試看別的影片。"
+                msgs.append(msg)
+
+    return  "\n".join(msgs)
+
+
+if __name__ == "__main__":
+
+    msg = get_response("https://www.youtube.com/watch?v=0exeq7-rZZc")
+    print(msg)
+        
